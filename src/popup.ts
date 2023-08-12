4acf3d163462a775777d895b35000959da8aaa04
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2023-08-12 22:10:41 +0900

    update popup

--- src/popup.ts
+++ src/popup.ts
@@ -16,28 +16,36 @@ const niconama = new Niconama(nicoapi);
 const programs = await niconama.getOnAirPrograms();
 console.log(programs);
 
-const div = document.getElementById("main");
-if (div != null) {
+const gridContainer = document.getElementById("main");
+if (gridContainer != null) {
   programs
     .map((program) => {
+      const item = document.createElement("div");
+      item.className = "grid-item";
+
       const link = document.createElement("a");
       link.href = program.watchPageUrl;
       link.onclick = async function () {
         console.log("onclick");
         await chrome.tabs.create({ active: true, url: program.watchPageUrl });
       };
+
       const img = document.createElement("img");
       img.src = program.screenshotThumbnail.liveScreenshotThumbnailUrl;
       link.appendChild(img);
+
+      const br1 = document.createElement("br");
       const span = document.createElement("span");
       span.textContent = program.title;
-      const br1 = document.createElement("br");
       const br2 = document.createElement("br");
-      return [link, br1, span, br2];
+
+      item.appendChild(link);
+      item.appendChild(br1);
+      item.appendChild(span);
+      item.appendChild(br2);
+      return item;
     })
-    .forEach((elements) => {
-      elements.forEach((element) => {
-        div.appendChild(element);
-      });
+    .forEach((element) => {
+      gridContainer.appendChild(element);
     });
 }
