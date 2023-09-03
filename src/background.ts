bec7d66ad22487ee6f41df85b8d50fb02fd8dc98
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2023-09-03 11:17:59 +0900

    fix incorrect background initialization way

--- src/background.ts
+++ src/background.ts
@@ -6,21 +6,34 @@ import { configureDefaultContainer } from "./di/register";
 
 // Receives keep-alive message from offscreen window just to wake up the background script.
 // https://stackoverflow.com/a/66618269
-function configureKeepAliveReceiver() {
+function configureKeepAliveListener() {
   self.onmessage = (event) => {
     console.log(new Date(), "onmessage", event);
   };
 }
 
-function configureChromeNotifications(background: Background) {
+function configureNotificationsListener(background: Background) {
   chrome.notifications.onClicked.addListener(async (notificationId) => {
     // console.log(`notification clicked: ${notificationId}`);
     await background.openNotification(notificationId);
   });
 }
 
+function configureRuntimeListener(background: Background) {
+  chrome.runtime.onInstalled.addListener(async () => {
+    await background.run();
+  });
+  chrome.runtime.onStartup.addListener(async () => {
+    await background.run();
+  });
+}
+
+function configureListener(background: Background) {
+  configureKeepAliveListener();
+  configureNotificationsListener(background);
+  configureRuntimeListener(background);
+}
+
 configureDefaultContainer();
 const background = container.resolve<Background>(InjectTokens.Background);
-configureKeepAliveReceiver();
-configureChromeNotifications(background);
-await background.run();
+configureListener(background);
