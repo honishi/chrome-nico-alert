118aa701b8f0c0132bf85b5fc1853442dfc76a66
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2023-09-25 01:08:19 +0900

    add async username resolving

--- src/entry/option.ts
+++ src/entry/option.ts
@@ -12,19 +12,41 @@ async function renderPage() {
     return;
   }
   autoOpenContainer.innerHTML = "";
+
   const userIds = await option.getAutoOpenUserIds();
   for (const userId of userIds) {
     const userIdDiv = await createUserIdDiv(userId);
     autoOpenContainer.appendChild(userIdDiv);
   }
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(async (entry) => {
+      if (!entry.isIntersecting) {
+        return;
+      }
+      const userId = entry.target.getAttribute("user-id");
+      if (userId === null) {
+        return;
+      }
+      const text = entry.target.getElementsByClassName("user-name")[0] as HTMLSpanElement;
+      const userName = await option.getUserName(userId);
+      if (text === null || userName === null) {
+        return;
+      }
+      text.textContent = userName;
+      observer.unobserve(entry.target);
+    });
+  });
+  [...autoOpenContainer.children].forEach((userDiv) => observer.observe(userDiv));
 }
 
 async function createUserIdDiv(userId: string): Promise<HTMLElement> {
   const option = container.resolve<Option>(InjectTokens.Option);
-  const userName = await option.getUserName(userId);
 
   const div = document.createElement("div");
-  div.className = "user-div";
+  div.className = "auto-open-user-div";
+  div.setAttribute("user-id", userId);
+
   const button = document.createElement("button");
   button.className = "remove-button";
   button.textContent = "[削除]";
@@ -33,17 +55,20 @@ async function createUserIdDiv(userId: string): Promise<HTMLElement> {
     await option.disableAutoOpen(userId);
     div.remove();
   });
+  div.appendChild(button);
+
   const text = document.createElement("span");
   text.className = "user-name";
-  text.textContent = userName;
+  text.textContent = ".....";
+  div.appendChild(text);
+
   const link = document.createElement("a");
   link.className = "user-link";
   link.text = `(${userId})`;
   link.href = `https://www.nicovideo.jp/user/${userId}`;
   link.target = "_blank";
-  div.appendChild(button);
-  div.appendChild(text);
   div.appendChild(link);
+
   // console.log(div);
   return div;
 }
