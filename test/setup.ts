c3e95def6669c34a77266a634ef7163c62b606b6
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2025-09-22 19:32:39 +0900

    remove ts-ignore with proper type declaration

--- test/setup.ts
+++ test/setup.ts
@@ -5,5 +5,10 @@
 // Polyfill for crypto in Node.js environment
 import { webcrypto } from "crypto";
 
-// @ts-ignore
-global.crypto = webcrypto as Crypto;
+// Properly type the global crypto
+declare global {
+  // eslint-disable-next-line no-var
+  var crypto: Crypto;
+}
+
+global.crypto = webcrypto as unknown as Crypto;
